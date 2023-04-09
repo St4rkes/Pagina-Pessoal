edccaeffca1d4b57a5cc8b8d5b7f35abf2519e05
Moisés Fraga <114237772+St4rkes@users.noreply.github.com>
2023-04-08 23:38:26 -0300

    Add files via upload

---
+++ paginaPerfil.js
@@ -0,0 +1,4 @@
+function abreConteudo(){
+    document.getElementById("conteudo").innerHTML = 
+        "<p> Ola sou Moises Fraga, estudande de Analise e desenvolvimento sistemas.<br> Ingressei no curso no ano de 2022 na faculdade SENAC-GO</p>";
+}
